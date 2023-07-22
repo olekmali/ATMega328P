3c8a5f4e49d2add1556483be99eeb2683a4711a9
Aleksander Malinowski <olekmali@ieee.org>
2023-07-22 00:41:40 -0500

    MPU6050 initialization improved by adding reset

--- 71_i2c_6050_simple/i2c_6050_simple/lib_mpu6050.c
+++ 71_i2c_6050_simple/i2c_6050_simple/lib_mpu6050.c
@@ -1,25 +1,39 @@
-//* Simple MPU6050 library - implementaiton file - 2021 *
+//* Simple MPU6050 library - implementation file - 2023 *
 #include "lib_mpu6050.h"
 #include "bios_twi_master.h"
 
-#define MPU6050_ADDR	0x68
+#define F_CPU 16000000UL
+#include <util/delay.h>
+
+#define MPU6050_ADDR        0x68
 
 /* MPU6050 register address */
-#define ACCEL_XOUT_H	0x3B
-#define ACCEL_XOUT_L	0x3C
-#define ACCEL_YOUT_H	0x3D
-#define ACCEL_YOUT_L	0x3E
-#define ACCEL_ZOUT_H	0x3F
-#define ACCEL_ZOUT_L	0x40
-#define PWR_MGMT_1		0x6B
+#define ACCEL_XOUT_H        0x3B
+#define ACCEL_XOUT_L        0x3C
+#define ACCEL_YOUT_H        0x3D
+#define ACCEL_YOUT_L        0x3E
+#define ACCEL_ZOUT_H        0x3F
+#define ACCEL_ZOUT_L        0x40
+#define SIGNAL_PATH_RESET   0x68
+#define PWR_MGMT_1          0x6B
+#define PWR_MGMT_2          0x6C
 
 
 void mpu_init(void)
 {
-    // Write 0 to PWR_MGMT_1 reg to wakeup MPU
-    uint8_t data[2] = {PWR_MGMT_1, 0};
-    ret_code_t error_code = tw_master_transmit(MPU6050_ADDR, data, sizeof(data), false);
-    if (error_code!=SUCCESS) { /* if desired, insert code for reporting the error */ }
+    uint8_t data1[2] = {PWR_MGMT_1, 0b10000000};        // reset device
+    ret_code_t e1 = tw_master_transmit(MPU6050_ADDR, data1, sizeof(data1), false);
+    _delay_ms(100);
+    uint8_t data2[2] = {SIGNAL_PATH_RESET, 0b00000111}; // reset signal path
+    ret_code_t e2 = tw_master_transmit(MPU6050_ADDR, data2, sizeof(data2), false);
+    _delay_ms(100);
+    uint8_t data3[2] = {PWR_MGMT_1, 0b00000000};        // wake up the device
+    ret_code_t e3 = tw_master_transmit(MPU6050_ADDR, data3, sizeof(data3), false);
+    uint8_t data4[2] = {PWR_MGMT_2, 0b00000111};        // disable gyroscopes and save 3.4 mA
+    ret_code_t e4 = tw_master_transmit(MPU6050_ADDR, data4, sizeof(data4), false);
+    if ( (e1!=SUCCESS) || (e2!=SUCCESS) || (e3!=SUCCESS) || (e4!=SUCCESS) ) {
+        /* if desired, insert code for reporting the error */
+    }
 }
 
 static void mpu_get_accel_raw(mpu_data_t* mpu_data) {
