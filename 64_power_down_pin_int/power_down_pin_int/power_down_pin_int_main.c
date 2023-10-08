5e643525dd5ff693d53e10b96ab3f88f60522c6a
Aleksander Malinowski <olekmali@ieee.org>
2023-10-07 23:18:58 -0500

    Simplified the example for Ping Changed interrupt waking up from sleep

--- 64_power_down_pin_int/power_down_pin_int/power_down_pin_int_main.c
+++ 64_power_down_pin_int/power_down_pin_int/power_down_pin_int_main.c
@@ -3,36 +3,22 @@
 #include <avr/interrupt.h>
 #include <avr/sleep.h>
 
+#define F_CPU (16000000UL)
+#include <util/delay.h>
+
 #include "bios_pinc_int.h"
 #include "bios_leds.h"
 
-//------------------------------------------------------------------------------------
-// HAL Functions
-//------------------------------------------------------------------------------------
-inline void sleep_On() {
-    //  set_sleep_mode(SLEEP_MODE_PWR_DOWN);
-    set_sleep_mode(SLEEP_MODE_PWR_SAVE);
-    sleep_enable();
-    sleep_cpu();
-}
-
-inline void sleep_Off() {
-    sleep_disable();
+void MyInterruptPinChangedFunction() {
+    // Note: this is called from an interrupt
+    //       do not cause any significant delay
+    sleep_disable(); // <-- important! or else program will fall asleep on exiting the interrupt
 }
 
 
 //------------------------------------------------------------------------------------
-// Pin Changed Interrupt Functionality
+// HAL Functions for Port C pins
 //------------------------------------------------------------------------------------
-void MyInterruptPinChangedFunction () {
-    // this is an example that just does something visible
-    // replace with your application code as needed
-    uint8_t t = leds_get();
-    t = t ^ B_L0;
-    leds_set( t );
-
-}
-
 
 void setup_PC012() {
     DDRC =  DDRC  & 0b11111000; // don't touch the upper 5 pin setup, just make sure that the lowest 3 pins are input/zero
@@ -40,6 +26,11 @@ void setup_PC012() {
 
     // enable the interrupt on the lowest 3 pins of PORTC
     PCINT_PINC_initialize( 0b00000111, MyInterruptPinChangedFunction );   // enable pin change interrupt on PINC.0
+}
+
+uint8_t get_PC012() {
+    return( PINC & 0b00000111);
+    // Note: for the four pushbuttons we typically use that would be: return( ( PIND >> 4 ) & 0x0F );
 }
 
 void setup_Unused_Pins() {
@@ -65,19 +56,31 @@ int main(void)
     setup_PC012();
     setup_Unused_Pins();
 
+    set_sleep_mode(SLEEP_MODE_PWR_DOWN);
+    //  set_sleep_mode(SLEEP_MODE_PWR_SAVE);
+
     sei(); // enable all interrupts
     while (1)
     {
-      sleep_On();
+        sleep_enable();
+        sleep_cpu();
 
         {   // the code fragment below is for illustration purposes only
             // it indicates that the main loop is running
-            cli(); leds_set( leds_get() | B_L4 ); sei();
-            for (volatile uint32_t i =0; i<30000; i++) ;
-            cli(); leds_set( leds_get() & ~B_L4 ); sei();
-            for (volatile uint32_t i =0; i<30000; i++) ;
+            uint8_t wakeup_reason = get_PC012();
+            if ( ( wakeup_reason & 0b00000001 ) == 0 ) {
+                // button on pin 0 was just pressed down 1->0
+                // or is being pressed down while another button changed
+                leds_set( leds_get() | B_L0 );
+                _delay_ms(100);
+                leds_set( 0 );
+            } else {
+                // any other button changed or pin 0 button released
+                leds_set( leds_get() | B_L1 );
+                _delay_ms(100);
+                leds_set( 0 );
+            }
         }
 
     }
 }
-
